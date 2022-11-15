d5e3511e37aab66bebedce5a1e2554b35b14a6e9
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-11-16 02:13:17 +0530

    Added : Welcome to Employee Wage Computation Program

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz.EmployeeWageProgram;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
